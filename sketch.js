5a9d83111a38b4bec79e5c26a6025b1d60d4a244
Bunny <78802546+muraliseasons4@users.noreply.github.com>
2021-05-31 16:53:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -101,7 +101,7 @@ function draw() {
     trex.velocityY = trex.velocityY + 0.8
   
     if (ground.x < 0){
-      ground.x = ground.width/2;
+      ground.x = ground.width/4;
     }
   
     trex.collide(invisibleGround);
@@ -136,6 +136,8 @@ function draw() {
     }
   }
   
+  camera.position.x=trex.x;
+  camera.position.y=trex.y;
   
   drawSprites();
 }
